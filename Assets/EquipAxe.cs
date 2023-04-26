c7581fefd2019bcf28561e85dfa3ba683af93646
Johannes Hinterholzer <j.hinterholzer@htlkrems.at>
2023-04-26 09:26:22 +0200

    leichter pick up jetz, keine ahnung wie drop
    lol

--- Assets/EquipAxe.cs
+++ Assets/EquipAxe.cs
@@ -4,51 +4,22 @@ using UnityEngine;
 
 public class EquipAxe : MonoBehaviour
 {
-    public GameObject Axe;
-    public Transform AxeParent;
-    // Start is called before the first frame update
+    public GameObject AxeOn;
+
     void Start()
     {
-        Axe.GetComponent<Rigidbody>().isKinematic = true;
+        AxeOn.SetActive(false);
     }
-    void Update()
-    {
-        //if (Input.GetKey(KeyCode.F))
-        //{
-        //    Drop();
-        //}
-    }
-    //void Drop()
-    //{
-    //    AxeParent.DetachChildren();
-    //    Axe.transform.eulerAngles = new Vector3(Axe.transform.position.x);
-    //    Axe.GetComponent<Rigidbody>().isKinematic = false;
-    //    Axe.GetComponent<MeshCollider>().enabled = true;
-    //}
 
-
-    void Equip()
-    {
-        Axe.GetComponent<Rigidbody>().isKinematic = true;
-
-        Axe.transform.position = AxeParent.transform.position;
-        Axe.transform.rotation = AxeParent.transform.rotation;
-
-        Axe.GetComponent<MeshCollider>().enabled = false;
-        Axe.transform.SetParent(AxeParent);
-
-    }
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             if (Input.GetKey(KeyCode.E))
             {
-                Equip();
+                this.gameObject.SetActive(false);
+                AxeOn.SetActive(true);
             }
         }
     }
 }
-
-
-
