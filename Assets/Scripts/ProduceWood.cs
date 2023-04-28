44a80c25703f443942a0467bd936a457385a98ea
Johannes Hinterholzer <j.hinterholzer@htlkrems.at>
2023-04-28 18:45:38 +0200

    Holzabbauen funkt. Probleme: nur ein Holz in einer Periode machen +  Abbauanimation aufrufen

--- Assets/Scripts/ProduceWood.cs
+++ Assets/Scripts/ProduceWood.cs
@@ -1,32 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class ProduceWood : MonoBehaviour
 {
     public GameObject Palette;
+    public GameObject Holz;
     public GameObject character_AxeOn;
-    
+    public float radius = 5f;
+    public GameObject MainCharacter;
+    public GameObject BucketCharacter;
+
     // Start is called before the first frame update
     void Start()
     {
-        character_AxeOn.SetActive(true);
+        Holz.SetActive(false);
     }
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Player")
-    //    {
-    //        if (character_AxeOn.transform.position = Palette.transform.position;)
-    //        {
-    //            Abbauen();
-    //        }
-    //    }
-    //}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && gameObject != MainCharacter.activeInHierarchy && gameObject != BucketCharacter.activeInHierarchy)
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            if (distance <= radius)
+            {
+                InvokeRepeating("Spawn", 0f, 5f);
+            }
+        }
+    }
+    
+    public void Spawn()
+    {
+        
+        Holz.SetActive(true);
+        Instantiate(Holz, Holz.transform.position, Holz.transform.rotation);
 
-    //public void Abbauen()
-    //{
-
-    //}
+    }
 }
